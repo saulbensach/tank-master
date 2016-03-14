c6f716267f41ec4e377c9745d939dc5d1aa66d8c
saulbensach <saulcube123@gmail.com>
2016-03-14 15:34:31 +0100

    Clases de los menus creadas y celltype

---
+++ core/src/com/bensach/saul/menu/RegisterMenu.java
@@ -0,0 +1,44 @@
+package com.bensach.saul.menu;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by saul- on 14/03/2016.
+ */
+public class RegisterMenu implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/bensach/saul/world/CellType.java
@@ -0,0 +1,11 @@
+package com.bensach.saul.world;
+
+/**
+ * Created by saul- on 14/03/2016.
+ */
+
+public enum CellType {
+
+    floor,wall
+
+}
